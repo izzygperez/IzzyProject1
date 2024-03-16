5ba7f34407d1387dcaeeacad3dc6f5cf36d71a18
SCU Imaginarium (Red) <126543406+izzygperez@users.noreply.github.com>
2024-03-15 17:11:48 -0700

    score
    connected score board to ball

--- Assets/02_Scripts/hoopCensor.cs
+++ Assets/02_Scripts/hoopCensor.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class hoopCensor : MonoBehaviour
 {
     public GameObject effectObject;
+    //reference to score script
+    int score = 0;
+    [SerializeField] score scoreScript;
 
     private void Start()
     {
         effectObject.SetActive(false);
+        scoreScript.textVarible = "score: " + score;
     }
 
     void OnTriggerEnter(Collider other)
@@ -16,6 +20,12 @@ public class hoopCensor : MonoBehaviour
         if(other.gameObject.tag == "basketball")
         {
             effectObject.SetActive(true);
+            score = score + 1;
+            scoreScript.textVarible = "score: "+score;    //set score script's textVariable to the score int as a string
+            if(score == 3)          //if score is 3 
+            {
+
+            }
         }
     }
 }
---
+++ Assets/02_Scripts/score.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public static score instance;
+
+    public TMP_Text canvasText;
+
+    public string textVarible = "score: ";
+
+    void Start()
+    {
+        canvasText.text = textVarible;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canvasText.text = textVarible;
+    }
+}
